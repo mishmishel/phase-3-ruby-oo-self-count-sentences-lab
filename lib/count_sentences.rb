d8339d6fc0de5e2c9eb928d53adc56885d450432
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-09-08 09:08:45 +1000

    commit tree 515313d7dae148fa0269a1de5da76c4766d69216

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    !!(self =~ /\.$/)
   end
 
   def question?
-
+    !!(self =~ /\?$/)
   end
 
   def exclamation?
-
+    !!(self =~ /\!$/)
   end
 
   def count_sentences
-
+    sentences = self.split(/[.!?]+\s*/)
+    
+    # Return the count of sentences.
+    sentences.size
   end
 end
